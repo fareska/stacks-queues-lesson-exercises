eeab45421041a1c103f1dc809c5d0985825408d0
fareska <fares.xb@gmail.com>
2020-12-14 18:19:22 +0200

    finished the stack class-spot check

--- src/Stack.js
+++ src/Stack.js
@@ -1,5 +1,36 @@
 /* Write your class below */
+class Stack {
+    constructor(){
+        this.stack=[]
+        this.length=0
+    }
 
+    print(){
+        console.log(this.stack)
+    }
+    
+    push(item){
+        this.stack[this.stack.length] =  item 
+        this.length++
+    }
+
+    isEmpty(){
+        if(!this.length>0){return true}
+        else{return false}
+    }
+
+    peek(){
+        return this.stack[this.stack.length-1]
+    }
+
+    pop(){
+        const removedItem = this.stack[this.stack.length-1]
+        delete this.stack[this.stack.length-1]
+        this.length--
+        return removedItem 
+    }
+
+}
 
 
 
@@ -8,8 +39,20 @@
 
 
 //You can paste the test code from the lesson below to test your solution
-
-
+let myStack = new Stack()
+console.log(myStack.isEmpty())    //true
+myStack.print()                    //[]
+myStack.push(2)      
+console.log(myStack.isEmpty())     //false
+myStack.push(4)      
+myStack.print()                    //[2,4]
+console.log(myStack.peek())        //4
+myStack.pop()
+myStack.pop()
+console.log(myStack.peek())       //null
+console.log(myStack.isEmpty())    //true
+/*
+*/
 
 
 
